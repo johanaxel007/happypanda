3d603fe145af353cbaa0ab50b159d1bc259842d8
johanaxel007 <axel.van.gestel@hotmail.be>
2026-09-10 17:58:00 +0200

    Refactored: version/misc_db.py onto the Qt6 enum spelling

--- version/misc_db.py
+++ version/misc_db.py
@@ -119,10 +119,10 @@ class NoTooltipModel(QIdentityProxyModel):
         super().__init__(parent)
         self.setSourceModel(model)
 
-    def data(self, index, role=Qt.DisplayRole):
-        if role == Qt.ToolTipRole:
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        if role == Qt.ItemDataRole.ToolTipRole:
             return None
-        if role == Qt.DecorationRole:
+        if role == Qt.ItemDataRole.DecorationRole:
             return app_constants.ARTIST_ICON
         return self.sourceModel().data(index, role)
 
@@ -145,7 +145,7 @@ class UniqueInfoModel(QSortFilterProxyModel):
                 if unique:
                     if not unique in self._unique:
                         if self.custom_filter != None:
-                            if not idx.data(Qt.UserRole + 1) in self.custom_filter:
+                            if not idx.data(Qt.ItemDataRole.UserRole + 1) in self.custom_filter:
                                 return False
                         self._unique.add(unique)
                         return True
@@ -163,7 +163,7 @@ class ListDelegate(QStyledItemDelegate):
     
     def sizeHint(self, option, index):
         size = super().sizeHint(option, index)
-        if index.data(Qt.DisplayRole) == self.create_new_list_txt:
+        if index.data(Qt.ItemDataRole.DisplayRole) == self.create_new_list_txt:
             return size
         return QSize(size.width(), size.height() * 2)
 
@@ -294,7 +294,7 @@ class TagsTreeView(QTreeWidget):
             for tag in tags[ns]:
                 child_item = QTreeWidgetItem(top_item)
                 child_item.setText(0, tag)
-        self.sortItems(0, Qt.AscendingOrder)
+        self.sortItems(0, Qt.SortOrder.AscendingOrder)
 
 class GalleryListEdit(misc.BasePopup):
     apply = pyqtSignal()
@@ -442,7 +442,7 @@ class GalleryLists(QListWidget):
     def create_new_list(self, name=None, gallery_list=None):
         new_item = misc.CustomListItem()
         self._in_proccess_item = new_item
-        new_item.setFlags(new_item.flags() | Qt.ItemIsEditable)
+        new_item.setFlags(new_item.flags() | Qt.ItemFlag.ItemIsEditable)
         new_item.setIcon(QIcon(app_constants.LIST_ICON))
         self.insertItem(0, new_item)
         if name:
@@ -486,12 +486,12 @@ class SideBarWidget(QFrame):
         super().__init__(parent)
         self.setAcceptDrops(True)
         self.parent_widget = parent
-        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.MinimumExpanding)
+        self.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.MinimumExpanding)
         self._widget_layout = QHBoxLayout(self)
 
         # widget stuff
         self._d_widget = QWidget(self)
-        self._d_widget.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.MinimumExpanding)
+        self._d_widget.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.MinimumExpanding)
         self._widget_layout.addWidget(self._d_widget)
         self.main_layout = QVBoxLayout(self._d_widget)
         self.main_layout.setSpacing(0)
@@ -581,7 +581,7 @@ class SideBarWidget(QFrame):
 
         self.slide_animation = misc.create_animation(self, "maximumSize")
         self.slide_animation.stateChanged.connect(self._slide_hide)
-        self.slide_animation.setEasingCurve(QEasingCurve.InOutQuad)
+        self.slide_animation.setEasingCurve(QEasingCurve.Type.InOutQuad)
 
     def _slide_hide(self, state):
         size = self.sizeHint()
@@ -631,10 +631,10 @@ class DBOverview(QWidget):
     about_to_close = pyqtSignal()
     def __init__(self, parent, window=False):
         if window:
-            super().__init__(None, Qt.Window)
+            super().__init__(None, Qt.WindowType.Window)
         else:
             super().__init__(parent)
-        self.setAttribute(Qt.WA_DeleteOnClose)
+        self.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose)
         self.parent_widget = parent
         main_layout = QVBoxLayout(self)
         tabbar = QTabWidget(self)
